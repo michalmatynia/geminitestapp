e02e2275b18ef5da74ee3da6cbc22ad7247ae8bd
Michal <mmatynia@gmail.com>
2026-01-14 06:18:48 +0100

    joi

--- app/(admin)/admin/cms/pages/[id]/edit/layout.tsx
+++ app/(admin)/admin/cms/pages/[id]/edit/layout.tsx
@@ -3,11 +3,22 @@
 import * as React from "react";
 import AdminLayout from "@/app/(admin)/layout";
 import CmsSideMenu from "@/components/cms/CmsSideMenu";
-import { CmsPageProvider, useCmsPage } from "./cmsPageContext";
 
-function CmsLayoutInner({ children }: { children: React.ReactNode }) {
-  const { page, setPage } = useCmsPage();
+interface Page {
+  id: string;
+  name: string;
+  components: any[];
+}
 
+export default function CmsLayout({
+  children,
+  page,
+  setPage,
+}: {
+  children: React.ReactNode;
+  page: Page | null;
+  setPage: React.Dispatch<React.SetStateAction<Page | null>>;
+}) {
   return (
     <AdminLayout>
       <div className="flex h-screen bg-gray-900 text-white">
@@ -15,13 +26,5 @@ function CmsLayoutInner({ children }: { children: React.ReactNode }) {
         <main className="flex-1 p-4 overflow-y-auto">{children}</main>
       </div>
     </AdminLayout>
-  );
-}
-
-export default function CmsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <CmsPageProvider>
-      <CmsLayoutInner>{children}</CmsLayoutInner>
-    </CmsPageProvider>
   );
 }
